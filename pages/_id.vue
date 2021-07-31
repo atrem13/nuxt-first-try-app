058594db2e7d1c92c956116a472f646b2a786f20
atrem13 <mertayoga17.my@gmail.com>
2021-07-31 15:59:39 +0800

    first try

---
+++ pages/_id.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <h1 class="directory__name">{{directory.name}}</h1>
+        <p class="directory__info">{{directory.info}}</p>
+    </section>
+</template>
+<script>
+import {mapState} from 'vuex'
+export default {
+    name: "directory-info",
+    computed: {
+        ...mapState(["directory"])
+    },
+    data(){
+        return{
+            // directory: this.$route.params.dir
+        };
+    }
+}
+</script>
